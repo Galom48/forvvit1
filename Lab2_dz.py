7d2dc0b19064c88fd5d98b2756693a1bfb36b675
Galom48 <antonkarman2@gmail.com>
2022-04-07 06:10:59 -0700

    lab2_dz

---
+++ Lab2_dz.py
@@ -0,0 +1,33 @@
+import requests
+
+s_city = "Moscow,RU"
+appid = "2798864358e1ab04ede0c6fa87d4acc9"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+
+data = res.json()
+
+# Погода в городе
+
+print("*" * 10, "Погода в городе", "*" * 10)
+print("Город: ", s_city)
+print("Погодные условия: ", data['weather'][0]['description'])
+print("Скорость ветра: ", data['wind']['speed'])
+print("Видимость: ", data['visibility'])
+print("Температура: ", data['main']['temp'])
+print("Минимальная температура: ", data['main']['temp_min'])
+print("Максимальная температура: ", data['main']['temp_max'])
+
+# Прогноз погоды на неделю
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("*" * 10, "Прогноз погоды на неделю", "*" * 10)
+
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),
+          "> \r\nПогодные условия <", i['weather'][0]['description'], "> \r\nСкорость ветра <",
+          i['wind']['speed'], "> \r\nВидимость <", i['visibility'], ">")
