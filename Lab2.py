5b7995d1e024e69769bab82678c9882153529ff7
Galom48 <antonkarman2@gmail.com>
2022-04-07 06:13:12 -0700

    lab2

--- Lab2.py
+++ Lab2.py
@@ -4,7 +4,7 @@ s_city = "Moscow,RU"
 appid = "2798864358e1ab04ede0c6fa87d4acc9"
 
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                   params = {'q':s_city,'units':'metric','lang':'ru','APPID':appid})
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 
 data = res.json()
 
@@ -20,11 +20,11 @@ print("Максимальная температура: ", data['main']['temp_ma
 # Прогноз погоды на неделю
 
 res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                   params = {'q':s_city,'units':'metric','lang':'ru','APPID':appid})
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
 
 print("*" * 10, "Прогноз погоды на неделю", "*" * 10)
 
 for i in data['list']:
-    print("Дата <", i['dt_txt'],"> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),
           "> \r\nПогодные условия <", i['weather'][0]['description'], ">")
